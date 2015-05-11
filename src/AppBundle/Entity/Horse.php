56fe61c7fba1fbf836d3c5916fae8ef091aa966c
Pawel Czuchaj <pawlo1501@gmail.com>
2015-05-11 21:21:30 +0200

    Horse entity

---
+++ src/AppBundle/Entity/Horse.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Horse
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Horse
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="lifespan", type="integer")
+     */
+    private $lifespan;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Horse
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lifespan
+     *
+     * @param integer $lifespan
+     * @return Horse
+     */
+    public function setLifespan($lifespan)
+    {
+        $this->lifespan = $lifespan;
+
+        return $this;
+    }
+
+    /**
+     * Get lifespan
+     *
+     * @return integer 
+     */
+    public function getLifespan()
+    {
+        return $this->lifespan;
+    }
+}
